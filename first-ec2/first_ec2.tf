ff962140d6377038bf9c9e026b1c1b37714e17b9
dylman123 <dylanklein29@gmail.com>
2022-09-07 13:35:02 -0400

    did terraform fmt

--- first-ec2/first_ec2.tf
+++ first-ec2/first_ec2.tf
@@ -1,23 +1,23 @@
 provider "aws" {
-    region = "ap-southeast-2"
-    access_key = var.my_access_key
-    secret_key = var.my_secret_key
+  region     = "ap-southeast-2"
+  access_key = var.my_access_key
+  secret_key = var.my_secret_key
 }
 
 # This data source finds the relevant ami per the specified AWS region above
 data "aws_ami" "my_ami" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
 }
 
 resource "aws_instance" "myec2" {
   # ami = "ami-0b55fc9b052b03618"
-  ami = data.aws_ami.my_ami.id
+  ami           = data.aws_ami.my_ami.id
   instance_type = "t2.micro"
 
   tags = {
