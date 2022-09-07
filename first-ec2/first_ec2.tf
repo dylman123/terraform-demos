0ab971a9c8318896cb1e67994da72e7f61623fe3
dylman123 <dylanklein29@gmail.com>
2022-09-07 13:30:39 -0400

    added data source to choose ami

--- first-ec2/first_ec2.tf
+++ first-ec2/first_ec2.tf
@@ -4,8 +4,20 @@ provider "aws" {
     secret_key = var.my_secret_key
 }
 
+# This data source finds the relevant ami per the specified AWS region above
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
 resource "aws_instance" "myec2" {
-  ami           = "ami-0b55fc9b052b03618"
+  # ami = "ami-0b55fc9b052b03618"
+  ami = data.aws_ami.my_ami.id
   instance_type = "t2.micro"
 
   tags = {
