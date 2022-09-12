9b677321fcd0e7350e174f4689054b14e20459bb
dylman123 <dylanklein29@gmail.com>
2022-09-12 19:23:48 -0400

    module output

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "my_ec2" {
     ami = "ami-0b55fc9b052b03618"
     # instance_type = "t2.micro"
     instance_type = var.instance_type
+
+    output "ec2_id" {
+        value = "aws_instance.my_ec2.id"
+    }
 }
