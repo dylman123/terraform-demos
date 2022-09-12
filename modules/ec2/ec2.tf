6fab0dec26b535e61e942f0688cd26ffe4daf633
dylman123 <dylanklein29@gmail.com>
2022-09-12 19:54:13 -0400

    introducing workspaces

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "my_ec2" {
     ami = "ami-0b55fc9b052b03618"
     # instance_type = "t2.micro"
-    instance_type = var.instance_type
+    instance_type = lookup(var.instance_type, terraform.workspace)
 
-    output "ec2_id" {
-        value = "aws_instance.my_ec2.id"
-    }
+    # output "ec2_id" {
+    #     value = "aws_instance.my_ec2.id"
+    # }
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,3 +1,8 @@
 variable "instance_type" {
-  default = "t2.micro"
+    type = map(string)
+    default = {
+        default = "t2.nano"
+        dev = "t2.micro"
+        prd = "t2.large"
+    }
 }
